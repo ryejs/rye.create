a968efcdd1dbb89d1997a8b4ec9445bc3db27cb9
Ricardo Tomasi <ricardobeat@gmail.com>
2013-02-12 21:19:11 -0200

    Add files

---
+++ rye.create.js
@@ -0,0 +1,170 @@
+// Rye.create
+// ----------
+
+// Hello.
+
+(function(name, deps, definition){
+    if (typeof module !== 'undefined') module.exports = definition(require)
+    else if (typeof define === 'function') define(name, deps, definition)
+    else (window.$ || window)[name] = definition(function(name){ return window[name] })
+})('create', [], function(require){
+
+    var _slice = Array.prototype.slice
+
+    var exp = {
+        operators  : /[>+]/g
+      , multiplier : /\*(\d+)$/
+      , id         : /#[\w-$]+/g
+      , tagname    : /^\w+/
+      , classname  : /\.[\w-$]+/g
+      , attributes : /\[([^\]]+)\]/g
+      , values     : /([\w-]+)(\s*=\s*(['"]?)([^,\]]+)(\3))?/g
+      , numbering  : /[$]+/g
+      , text       : /\{(.+)\}/
+    }
+
+    // Converts a documentFragment element tree to an HTML string. When a documentFragment
+    // is given to `.appendChild` it's *contents* are appended; we need to clone the
+    // fragment so that it remains populated and can still be used afer a `toHTML` call.
+    function toHTML () {
+        var div = document.createElement('div')
+        div.appendChild(this.cloneNode(true))
+        return div.innerHTML
+    }
+
+    // Pads number `n` with `ln` zeroes.
+    function pad (n, ln){
+        var n = n.toString()
+        while (n.length < ln) n = '0' + n
+        return n
+    }
+
+    // Replaces ocurrences of '$' with the equivalent padded index.
+    // `$$ == 01`, `$$$$ == 0001`
+    function numbered (value, n) {
+        return value.replace(exp.numbering, function(m){
+            return pad(n + 1, m.length)
+        })
+    }
+
+    // .create API
+    // ----------
+
+    function create (str, data) {
+
+        // Start by splitting the string into it's descendants and creating
+        // the fragment which will hold the result DOM tree.
+        var parts = str.split(exp.operators).map(Function.prototype.call, String.prototype.trim)
+          , tree = document.createDocumentFragment()
+          , ops = new RegExp(exp.operators)
+          , match
+
+        // Create a DOM element.
+        function Element(index, tag, id, className, text, attrs){
+            var element = document.createElement(tag)
+
+            if (id)        element.id = numbered(id, index)
+            if (className) element.className = numbered(className, index)
+            if (text)      element.appendChild(document.createTextNode(text))
+
+            if (attrs) for (var key in attrs) {
+                if (!attrs.hasOwnProperty(key)) continue
+                element.setAttribute(key, numbered(attrs[key], index))
+            }
+
+            return element
+        }
+
+        // At first the documentFragment is the only parent.
+        var parents = [tree]
+
+        // Parsing
+        // -------
+
+        // Go over the abbreviations one level at a time, and process
+        // corresponding element values
+        parts.forEach(function(original, i){
+
+            var part = original
+              , op = (ops.exec(str) || [])[0]
+              , count = 1
+              , tag
+              , id
+              , classes
+              , text
+              , attrs = {}
+
+            // #### Attributes
+            // Attributes are parsed first then removed so that it takes precedence
+            // over IDs and classNames for the `#.` characters.
+            if (match = part.match(exp.attributes)) {
+                var matched = match[match.length-1]
+                while(match = exp.values.exec(matched)){
+                    attrs[match[1]] = (match[4] || '').replace(/['"]/g, '').trim()
+                }
+                part = part.replace(exp.attributes, '')
+            }
+
+            // #### Multipliers
+            if (match = part.match(exp.multiplier)) {
+                var times = +match[1]
+                if (times > 0) count = times
+            }
+
+            // #### IDs
+            if (match = part.match(exp.id)) {
+                id = match[match.length-1].substr(1)
+            }
+
+            // #### Tag names
+            if (match = part.match(exp.tagname)) {
+                tag = match[0]
+            } else {
+                tag = 'div'
+            }
+
+            // #### Class names
+            if (match = part.match(exp.classname)) {
+                classes = match.map(function(c){
+                    return c.substr(1)
+                }).join(' ')
+            }
+
+            // #### Text
+            if (match = part.match(exp.text)) {
+                text = match[1]
+                if (data) {
+                    text = text.replace(/\$(\w+)/g, function(m, key){
+                        return data[key]
+                    })
+                }
+            }
+            
+            // Insert `count` copies of the element per parent
+            _slice.call(parents, 0).forEach(function(parent, parentIndex){
+                for (var index = 0; index < count; index++){
+                    // Use parentIndex if this element has a count of 1
+                    var _index = count > 1 ? index : parentIndex
+                    parent.appendChild(Element(_index, tag, id, classes, text, attrs))
+                }
+            })
+
+            // If the next operator is '>' replace `parents` with their childNodes for the next iteration
+            if (op === '>') {
+                parents = parents.reduce(function(p,c,i,a){
+                    return p.concat(_slice.call(c.childNodes, 0).filter(function(e){ return e.nodeType === 1 }))
+                }, [])
+            }
+
+        })
+
+        // Augment the documentFragment with the `toHTML` method.
+        tree.toHTML = toHTML
+
+        return tree
+    }
+
+    // Export main method
+    return create
+
+});
---
+++ test/spec.js
@@ -0,0 +1,227 @@
+var _slice = Array.prototype.slice
+
+function pad (n, ln){
+    var n = n.toString()
+    while (n.length < ln) n = '0' + n
+    return n
+}
+
+test('creates document fragment', function(){
+    var frag = $.create('div')
+    assert.instanceOf(frag, DocumentFragment)
+})
+
+test('tag', function(){
+    var div = $.create('div').firstChild
+    assert.instanceOf(div, Element)
+    assert.equal(div.tagName.toLowerCase(), 'div')
+})
+
+test('id', function(){
+    var div = $.create('div#test').firstChild
+    assert.instanceOf(div, Element)
+    assert.equal(div.id, 'test')
+})
+
+test('multiple ids', function(){
+    var div2 = $.create('div#one#two').firstChild
+    assert.instanceOf(div2, Element)
+    assert.equal(div2.id, 'two', 'last definition is used')
+})
+
+test('class', function(){
+    var div = $.create('div.test').firstChild
+    assert.instanceOf(div, Element)
+    assert.equal(div.className, 'test')
+})
+
+test('multiple classes', function(){
+    var div = $.create('div.one.two.three.four-five_six').firstChild
+    assert.instanceOf(div, Element)
+    assert.equal(div.className, 'one two three four-five_six')
+})
+
+test('id + classes', function(){
+    var div = $.create('div#hey.one.two.three').firstChild
+    assert.instanceOf(div, Element)
+    assert.equal(div.id, 'hey')
+    assert.equal(div.className, 'one two three')
+})
+
+test('id + classes interleaved', function(){
+    var div2 = $.create('div.one.two#hey.three').firstChild
+    assert.instanceOf(div2, Element)
+    assert.equal(div2.id, 'hey')
+    assert.equal(div2.className, 'one two three')
+})
+
+test('attributes', function(){
+    var div = $.create('div[title="hello",data-bacon=1]').firstChild
+    assert.equal(div.getAttribute('title'), 'hello')
+    assert.equal(div.getAttribute('data-bacon'), 1)
+})
+
+test('attributes with spaces', function(){
+    var div = $.create('div[title=" hello  world ",data-bacon=1]').firstChild
+    assert.equal(div.getAttribute('title'), 'hello  world', 'string is trimmed')
+    assert.equal(div.getAttribute('data-bacon'), 1)
+})
+
+test('attributes ignore extraneous spaces', function(){
+    var div = $.create('div[title = "hello you" ,data-bacon= 1]').firstChild
+    assert.equal(div.getAttribute('title'), 'hello you')
+    assert.equal(div.getAttribute('data-bacon'), 1)
+})
+
+test('empty attributes', function(){
+    var div = $.create('div[title]').firstChild
+    assert.equal(div.getAttribute('title'), '')
+})
+
+test('repetition', function(){
+    var frag = $.create('li*3')
+      , items = frag.childNodes
+
+    assert.lengthOf(items, 3)
+    assert.equal(items[0].tagName.toLowerCase(), 'li')
+})
+
+suite('operators', function(){
+
+    test('>', function(){
+        var frag = $.create('div > p > span')
+        assert.lengthOf(frag.querySelector('div').childNodes, 1)
+        assert.equal(frag.querySelector('div p').tagName.toLowerCase(), 'p')
+        assert.equal(frag.querySelector('div span').tagName.toLowerCase(), 'span')
+    })
+
+    test('+', function(){
+        var frag = $.create('div + p')
+        assert.lengthOf(frag.querySelector('div').childNodes, 0)
+        assert.equal(frag.querySelector('div + p').tagName.toLowerCase(), 'p')
+    })
+
+})
+
+suite('features', function(){
+
+    test('numbering', function(){
+        var lis = $.create('li.n$#l$*5').childNodes
+        assert.lengthOf(lis, 5)
+        _slice.call(lis).forEach(function(li, i){
+            assert.equal(li.className, 'n' + (i+1))
+            assert.equal(li.id, 'l' + (i+1))
+        })
+    })
+
+    test('numbering with padding', function(){
+        var lis = $.create('li.n$$#l$$$$*27').childNodes
+        assert.lengthOf(lis, 27)
+        _slice.call(lis).forEach(function(li, i){
+            assert.equal(li.className, 'n' + pad(i+1, 2))
+            assert.equal(li.id, 'l' + pad(i+1, 4))
+        })
+    })
+
+    test('numbering inherited from parent when single element', function(){
+        var ps = $.create('li*5 > p.n$').querySelectorAll('p')
+        assert.lengthOf(ps, 5)
+        _slice.call(ps).forEach(function(p, i){
+            assert.equal(p.className, 'n' + (i+1))
+        })
+    })
+
+    test('text', function(){
+        var div = $.create('div{hello}').firstChild
+        assert.equal(div.tagName.toLowerCase(), 'div')
+        assert.equal(div.textContent, 'hello')
+    })
+
+    test('text replacement', function(){
+        var div = $.create('div{hello $x$y}', { x: 'world', y: '!' }).firstChild
+        assert.equal(div.tagName.toLowerCase(), 'div')
+        assert.equal(div.textContent, 'hello world!')
+    })
+
+    test('text with siblings', function(){
+        var div = $.create('div{hello}>p*2').firstChild
+          , text = div.firstChild
+        assert.ok(text)
+        assert.equal(text.nodeType, 3)
+        assert.equal(text.textContent, 'hello')
+        assert.equal(div.querySelectorAll('p').length, 2)
+    })
+
+    test('toHTML', function(){
+        var html = $.create('div').toHTML()
+        assert.equal(html.toLowerCase(), '<div></div>')
+    })
+
+    test('toHTML complex', function(){
+        var html = $.create('ul#oi > li.x*2 > a').toHTML()
+        assert.equal(html.toLowerCase(), '<ul id="oi"><li class="x"><a></a></li><li class="x"><a></a></li></ul>')
+    })
+
+    test('toHTML preserves fragment contents', function(){
+        var frag = $.create('div'), html
+        frag.toHTML()
+        html = frag.toHTML()
+        assert.equal(html.toLowerCase(), '<div></div>')
+    })
+
+})
+
+suite('samples', function(){
+
+    test('list', function(){
+        var frag = $.create('div{hello}#main>ul.list.bacon#bacon > li.hello$*4 > a[href=#$]{hello $x}', { x: 'world' })
+
+        assert.equal(frag.toHTML(), '' +
+            '<div id="main">' +
+                'hello' +
+                '<ul id="bacon" class="list bacon">' +
+                    '<li class="hello1"><a href="#1">hello world</a></li>' +
+                    '<li class="hello2"><a href="#2">hello world</a></li>' +
+                    '<li class="hello3"><a href="#3">hello world</a></li>' +
+                    '<li class="hello4"><a href="#4">hello world</a></li>' +
+                '</ul>' +
+            '</div>'
+        )
+
+    })
+
+})
+
+// Tests imported from emmet
+suite('emmet', function(){
+
+    function compare(abbreviation, expanded){
+        var html = $.create(abbreviation).toHTML()
+        assert.equal(html, expanded)
+    }
+
+    test("'+' operator", function() {
+        compare('p+p', '<p></p><p></p>')
+        compare('p.name+p+p', '<p class="name"></p><p></p><p></p>')
+    })
+
+    test("'>' operator", function() {
+        compare('p>em', '<p><em></em></p>')
+        compare('p.hello>em.world>span', '<p class="hello"><em class="world"><span></span></em></p>')
+    })
+
+    test('attributes', function(){
+        compare('span[title]', '<span title=""></span>')
+        compare('span[title data]', '<span title="" data=""></span>')
+        compare('span.test[title data]', '<span class="test" title="" data=""></span>')
+        compare('span#one.two[title data]', '<span id="one" class="two" title="" data=""></span>')
+        compare('span[title=Hello]', '<span title="Hello"></span>')
+        compare('span[title="Hello world"]', '<span title="Hello world"></span>')
+        compare('span[title=\'Hello world\']', '<span title="Hello world"></span>')
+        compare('span[title="Hello world",data=other]', '<span title="Hello world" data="other"></span>')
+        compare('span[title="Hello world",data=other,attr2,attr3]', '<span title="Hello world" data="other" attr2="" attr3=""></span>')
+        compare('span[title="Hello world",data=other,attr2,attr3]>em', '<span title="Hello world" data="other" attr2="" attr3=""><em></em></span>')
+        compare('filelist[id=javascript.files]', '<filelist id="javascript.files"></filelist>')
+    })
+
+})
